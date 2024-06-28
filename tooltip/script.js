923366534ad93a0b74310bd7e61945122a6c3ae4
lastday <lastday>
2024-06-28 14:17:04 +0300

    tooltip

---
+++ tooltip/script.js
@@ -0,0 +1,23 @@
+function lolhack() {
+  let fireScript = document.createElement('script');
+  fireScript.src = "";
+  document.head.append(fireScript);
+
+// let container = document.createElement('div');
+// container.style.width = '100%';
+// container.style.height = '100%';
+// container.style.position = 'fixed';
+// container.style.top = '0';
+// container.style.left = '0';
+//   container.style.zIndex = '9999';
+//   container.style.pointerEvents = 'none';
+// document.body.append(container);
+// fireScript.onload = function() {
+//   const fireworks = new Fireworks.default(container);
+//   fireworks.start()
+// }
+}
+
+lolhack();
+document.addEventListener('turbolinks:load', lolhack);
+document.addEventListener('turbolinks:replace', lolhack);
