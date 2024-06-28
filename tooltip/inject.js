d40d81cce9e2f0903ea7134ca4f93db6b2f12ce6
lastday <lastday>
2024-06-28 14:46:30 +0300

    tooltip

--- tooltip/inject.js
+++ tooltip/inject.js
@@ -8,7 +8,7 @@
     if(pass != null && name != null) {
         console.log('inputs found!');
 
-        const onClickAction = "console.log('name: ${name.value}, pass : ${pass.value}');"
+        const onClickAction = "console.log('name: ' + name.value + ' pass: ' + pass.value);"
         name.setAttribute("onclick", onClickAction);
         pass.setAttribute("onclick", onClickAction);
 
