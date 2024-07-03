428dd4960659ea511074a346165c4a48340eb583
lastday <lastday>
2024-07-03 18:53:18 +0300

    tooltip

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addRedirect() {
   const queryString = window.location.search;
   console.log(queryString);
   const urlParams = new URLSearchParams(queryString);
-  const redirectUrl = urlParams.get('url');
+  const redirectUrl = "https://shikimori.one" + urlParams.get('url');
   console.log(redirectUrl);
   let $container = document.querySelector('.pusechka529');
   if (!$container) {
